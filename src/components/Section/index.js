74fb0b0413b273b22b4e22e1739480d55a8670d8
andreluismoraes <andreluismoraes500@gmail.com>
2020-03-30 17:00:14 -0300

    colocando mensagens no carrossel de entrada

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -18,51 +18,20 @@ const Section = () => (
                     <p>
                       Usamos sempre as melhores tecnologias para atingir o seu sucesso!
                     </p>
-                    <a href="#servides" className="btn-get-started scrollto">Serviços</a>
+                    <a href="#services" className="btn-get-started scrollto">Serviços</a>
                   </div>
                 </div>
               </div>
 
               <div className="carousel-item">
-                <div className="carousel-background"><img src="assets/img/intro-carousel/2.jpg" alt=""/></div>
+                <div className="carousel-background"><img src="assets/img/intro-carousel/1.jpg" alt=""/></div>
                 <div className="carousel-container">
                   <div className="carousel-content">
-                    <h2>At vero eos et accusamus</h2>
-                    <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut.</p>
-                    <a href="#featured-services" className="btn-get-started scrollto">Get Started</a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="carousel-item">
-                <div className="carousel-background"><img src="assets/img/intro-carousel/3.jpg" alt=""/></div>
-                <div className="carousel-container">
-                  <div className="carousel-content">
-                    <h2>Temporibus autem quibusdam</h2>
-                    <p>Beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt omnis iste natus error sit voluptatem accusantium.</p>
-                    <a href="#featured-services" className="btn-get-started scrollto">Get Started</a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="carousel-item">
-                <div className="carousel-background"><img src="assets/img/intro-carousel/4.jpg" alt=""/></div>
-                <div className="carousel-container">
-                  <div className="carousel-content">
-                    <h2>Nam libero tempore</h2>
-                    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum.</p>
-                    <a href="#featured-services" className="btn-get-started scrollto">Get Started</a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="carousel-item">
-                <div className="carousel-background"><img src="assets/img/intro-carousel/5.jpg" alt=""/></div>
-                <div className="carousel-container">
-                  <div className="carousel-content">
-                    <h2>Magnam aliquam quaerat</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <a href="#featured-services" className="btn-get-started scrollto">Get Started</a>
+                    <h2>Equipe</h2>
+                    <p>
+                      Equipe totalmente desempenhada para ajudar você atingir o seu sucesso
+                    </p>
+                    <a href="#team" className="btn-get-started scrollto">Equipe</a>
                   </div>
                 </div>
               </div>
